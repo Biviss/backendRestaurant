f4ee7574e3c494a88b35cddd342e5c677b5921c7
Biviss <babchenkoartem228@gmail.com>
2024-10-05 20:02:48 +0300

    Add all crud func and endpoints for menu items

--- app/crud/menus.py
+++ app/crud/menus.py
@@ -20,3 +20,28 @@ def create_menu_item(db: Session, menu_item: schemas.MenuItemCreate, restaurant_
 def get_menu_items(db: Session, restaurant_id: int) -> list[schemas.MenuItem]:
     menu_items = db.query(models.MenuItem).filter(models.MenuItem.restaurant_id == restaurant_id).all()
     return [schemas.MenuItem.from_orm(item) for item in menu_items]
+
+
+def update_menu_item(db: Session, menu_item_id: int, menu_item: schemas.MenuItemCreate) -> schemas.MenuItem | None:
+    db_menu_item = db.query(models.MenuItem).filter(models.MenuItem.id == menu_item_id).first()
+
+    if db_menu_item is None:
+        return None
+
+    for key, value in menu_item.dict().items():
+        setattr(db_menu_item, key, value)
+
+    db.commit()
+    db.refresh(db_menu_item)
+    return schemas.MenuItem.from_orm(db_menu_item)
+
+
+def delete_menu_item(db: Session, menu_item_id: int) -> schemas.MenuItem | None:
+    db_menu_item = db.query(models.MenuItem).filter(models.MenuItem.id == menu_item_id).first()
+
+    if db_menu_item is None:
+        return None
+
+    db.delete(db_menu_item)
+    db.commit()
+    return schemas.MenuItem.from_orm(db_menu_item)
--- app/routers/menus.py
+++ app/routers/menus.py
@@ -17,3 +17,19 @@ def create_menu_item(restaurant_id: int, menu_item: schemas.MenuItemCreate, db: 
 @router.get("/{restaurant_id}/menu/", response_model=list[schemas.MenuItem])
 def get_menu_items(restaurant_id: int, db: Session = Depends(database.get_db)):
     return menus.get_menu_items(db, restaurant_id=restaurant_id)
+
+
+@router.put("/menu/{menu_item_id}", response_model=schemas.MenuItem)
+def update_menu_item(menu_item_id: int, menu_item: schemas.MenuItemCreate, db: Session = Depends(database.get_db)):
+    db_menu_item = menus.update_menu_item(db, menu_item_id=menu_item_id, menu_item=menu_item)
+    if db_menu_item is None:
+        raise HTTPException(status_code=404, detail="Menu item not found")
+    return db_menu_item
+
+
+@router.delete("/menu/{menu_item_id}", response_model=schemas.MenuItem)
+def delete_menu_item(menu_item_id: int, db: Session = Depends(database.get_db)):
+    db_menu_item = menus.delete_menu_item(db, menu_item_id=menu_item_id)
+    if db_menu_item is None:
+        raise HTTPException(status_code=404, detail="Menu item not found")
+    return db_menu_item
