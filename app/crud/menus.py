6a751628831fe34f0b49885cd4aaa2db00738c6f
Biviss <babchenkoartem228@gmail.com>
2024-10-06 16:04:00 +0300

    Add commits for crud func

--- app/crud/menus.py
+++ app/crud/menus.py
@@ -5,6 +5,20 @@ from app.models import models
 
 
 def create_menu_item(db: Session, menu_item: menus.MenuItemCreate, restaurant_id: int) -> menus.MenuItem:
+    """
+        Creates a new menu item for the restaurant.
+
+        Args:
+            db (Session): The database session.
+            menu_item (menus.MenuItemCreate): Data for creating a new menu item.
+            restaurant_id (int): The ID of the restaurant.
+
+        Returns:
+            menus.MenuItem: The newly created menu item.
+
+        Raises:
+            ValueError: If the menu item already exists.
+        """
     db_menu_item = models.MenuItem(**menu_item.dict(), restaurant_id=restaurant_id)
     db.add(db_menu_item)
 
@@ -19,11 +33,32 @@ def create_menu_item(db: Session, menu_item: menus.MenuItemCreate, restaurant_id
 
 
 def get_menu_items(db: Session, restaurant_id: int) -> list[menus.MenuItem]:
+    """
+        Get all menu items for the restaurant.
+
+        Args:
+            db (Session): The database session.
+            restaurant_id (int): The ID of the restaurant.
+
+        Returns:
+            list[menus.MenuItem]: A list of menu items for the restaurant.
+        """
     menu_items = db.query(models.MenuItem).filter(models.MenuItem.restaurant_id == restaurant_id).all()
     return [menus.MenuItem.from_orm(item) for item in menu_items]
 
 
 def update_menu_item(db: Session, menu_item_id: int, menu_item: menus.MenuItemCreate) -> menus.MenuItem | None:
+    """
+        Updates the menu item.
+
+        Args:
+            db (Session): The database session.
+            menu_item_id (int): The ID of the menu item.
+            menu_item (menus.MenuItemCreate): The updated data for the menu item.
+
+        Returns:
+            menus.MenuItem | None: The updated menu item or None if the item was not found.
+        """
     db_menu_item = db.query(models.MenuItem).filter(models.MenuItem.id == menu_item_id).first()
 
     if db_menu_item is None:
@@ -38,6 +73,16 @@ def update_menu_item(db: Session, menu_item_id: int, menu_item: menus.MenuItemCr
 
 
 def delete_menu_item(db: Session, menu_item_id: int) -> menus.MenuItem | None:
+    """
+       Deletes a menu item.
+
+       Args:
+           db (Session): The database session.
+           menu_item_id (int): The ID of the menu item.
+
+       Returns:
+           menus.MenuItem | None: The deleted menu item or None if the item was not found.
+       """
     db_menu_item = db.query(models.MenuItem).filter(models.MenuItem.id == menu_item_id).first()
 
     if db_menu_item is None:
--- app/crud/restaurants.py
+++ app/crud/restaurants.py
@@ -5,11 +5,35 @@ from app.models import models
 
 
 def get_restaurants(db: Session, skip: int = 0, limit: int = 10) -> list[restaurants.Restaurant]:
+    """
+        Retrieves a list of restaurants.
+
+        Args:
+            db (Session): The database session.
+            skip (int, optional): Number of records to skip. Defaults to 0.
+            limit (int, optional): Maximum number of records to return. Defaults to 10.
+
+        Returns:
+            list[restaurants.Restaurant]: A list of restaurants within the specified range.
+        """
     restaurants_in = db.query(models.Restaurant).offset(skip).limit(limit).all()
     return [restaurants.Restaurant.from_orm(restaurant) for restaurant in restaurants_in]
 
 
 def create_restaurant(db: Session, restaurant: restaurants.RestaurantCreate) -> restaurants.Restaurant:
+    """
+        Creates a new restaurant.
+
+        Args:
+            db (Session): The database session.
+            restaurant (restaurants.RestaurantCreate): Data for creating a new restaurant.
+
+        Returns:
+            restaurants.Restaurant: The new created restaurant.
+
+        Raises:
+            ValueError: If the restaurant already exists in the database.
+        """
     db_restaurant = models.Restaurant(**restaurant.dict())
     db.add(db_restaurant)
 
@@ -23,11 +47,18 @@ def create_restaurant(db: Session, restaurant: restaurants.RestaurantCreate) -> 
     return restaurants.Restaurant.from_orm(db_restaurant)
 
 
-def get_restaurant(db: Session, restaurant_id: int):
-    return db.query(models.Restaurant).filter(models.Restaurant.id == restaurant_id).first()
-
-
 def update_restaurant(db: Session, restaurant_id: int, restaurant: restaurants.RestaurantCreate) -> models.Restaurant | None:
+    """
+        Updates the restaurant.
+
+        Args:
+            db (Session): The database session.
+            restaurant_id (int): The ID of the restaurant.
+            restaurant (restaurants.RestaurantCreate): The updated restaurant data.
+
+        Returns:
+            models.Restaurant | None: The updated restaurant object, or None if the restaurant was not found.
+        """
     db_restaurant = db.query(models.Restaurant).filter(models.Restaurant.id == restaurant_id).first()
 
     if db_restaurant is None:
@@ -42,6 +73,16 @@ def update_restaurant(db: Session, restaurant_id: int, restaurant: restaurants.R
 
 
 def delete_restaurant(db: Session, restaurant_id: int) -> models.Restaurant | None:
+    """
+        Deletes a restaurant.
+
+        Args:
+            db (Session): The database session.
+            restaurant_id (int): The ID of the restaurant.
+
+        Returns:
+            models.Restaurant | None: The deleted restaurant object, or None if the restaurant was not found.
+        """
     db_restaurant = db.query(models.Restaurant).filter(models.Restaurant.id == restaurant_id).first()
 
     if db_restaurant is None:
